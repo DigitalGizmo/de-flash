b3d6a52f5dd1773a3e604ecfe2fb9f3efe09ec34
Don Button <donpublic@digitalgizmo.com>
2020-07-20 17:10:20 -0400

    two ways to stop pop flicker

--- centuries/activities/dressup/js/dressup.js
+++ centuries/activities/dressup/js/dressup.js
@@ -4,13 +4,34 @@ var dressApp = new Vue({
 	el: '#app',
 	data: {
 		layerNum: 1,
-		popId: 'none'
+		popId: 'none',
+    mouseOnPop: false
 	},
 	methods: {
 		showPop: function(shortname) {
 			console.log(' -- showPop')
 			this.popId = shortname
 		},
+		hidePop: function() {
+			console.log(' -- hidePop')
+      // event.stopPropagation();
+      if (!this.mouseOnPop) {
+  			this.popId = 'none'
+      }
+		},
+    onPop: function(truth) {
+      console.log(' -- onPop: ' + truth)
+      this.mouseOnPop = truth
+      // this.popId = 'none'
+    },
+    leavePop: function() {
+      console.log(' -- leavePop')
+      // if (this.mouseOnPop) {
+        this.popId = 'none'
+        this.mouseOnPop = false
+
+      // }
+    },
 		prevLayer: function() {
 			this.popId = 'none'
 			// this.layerNum = this.layerNum++
