c9611cfa7a1b3a1932dc862718aeb14d0be0d936
Don Button <donpublic@digitalgizmo.com>
2020-07-28 10:33:19 -0400

    Initial audio
    layer-1 etc naming

--- centuries/activities/dressup/js/dressup.js
+++ centuries/activities/dressup/js/dressup.js
@@ -9,12 +9,34 @@ var dressApp = new Vue({
 		layerNum: 1,
     layerTitle: 'layer title',
 		popId: 'none',
+    audioObject: null,
+    audioOn: false,
+    personPath: '1660_wob_man'
 	},
 	methods: {
 		showPop: function(_popId) {
 			// console.log(' -- showPop')
+      // Stop any playing sound
+      if (this.audioObject != null) {
+        this.audioObject.pause()
+      }
 			this.popId = _popId
       this.popIsOpen = true
+      // Play audio, if on
+      if (this.audioOn && this.popId != 'none') {
+        const audioFileName = 'html5/' +
+          this.personPath + '/audio/' + this.popId + '.mp3'
+        console.log('  -- audio on, popId none')
+          console.log('  -- audioFileName:' + audioFileName)
+        // if(audioFileName.exists()) {
+        try {
+          this.audioObject = new Audio(audioFileName);
+          // this.audioObject.muted = false;;
+          this.audioObject.play();          
+        } catch (e) {
+          alert(audioFileName + ' not found')
+        }        
+      }
 		},
 		prevLayer: function() {
 			this.popId = 'none'
@@ -25,6 +47,17 @@ var dressApp = new Vue({
 			this.popId = 'none'
 			// this.layerNum = this.layerNum++
 			this.layerNum = 2
-		}
+		},
+    toggleAudio: function() {
+      this.audioOn ? this.audioOn = false : this.audioOn = true
+    }
+    // playSound (audioFileName) {
+    //   console.log(' -- audioFileName: ' + audioFileName)
+    //   if(this.audioOn && audioFileName) {
+    //     this.audioObject = new Audio(audioFileName);
+    //     this.audioObject.play();
+    //   }
+    // }
+
 	}
 });
